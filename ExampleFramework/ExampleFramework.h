5f7d27008dca6853dd1727de9b5c346d0bc35930
Erkut Demirhan <edemirhan@veridiumid.com>
2018-12-03 17:08:55 +0200

    Second commit

--- ExampleFramework/ExampleFramework.h
+++ ExampleFramework/ExampleFramework.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char ExampleFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ExampleFramework/PublicHeader.h>
 
+// Added new line
 
