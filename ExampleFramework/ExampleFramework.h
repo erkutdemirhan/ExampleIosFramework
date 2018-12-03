15eb20e7143148a574228ffd8259a0a39878fc91
Erkut Demirhan <erkutdemirhan@gmail.com>
2018-12-03 16:02:52 +0200

    Initial Commit
     Author:    Erkut Demirhan <erkutdemirhan@gmail.com>

---
+++ ExampleFramework/ExampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  ExampleFramework.h
+//  ExampleFramework
+//
+//  Created by Veridium on 03/12/2018.
+//  Copyright © 2018 Veridium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExampleFramework.
+FOUNDATION_EXPORT double ExampleFrameworkVersionNumber;
+
+//! Project version string for ExampleFramework.
+FOUNDATION_EXPORT const unsigned char ExampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleFramework/PublicHeader.h>
+
+
